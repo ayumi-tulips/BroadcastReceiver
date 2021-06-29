f76e3f82b26a5e2f66b2ad8dfe27116e6e9d5ee5
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-06-29 22:53:32 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ayumi/broadcastreceiver/TimezoneBrodcastReceiver.kt
@@ -0,0 +1,15 @@
+package jp.techacademy.ayumi.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class TimezoneBrodcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("TimezoneBrodcastReceiver.onReceive() is not implemented")
+        Toast.makeText(context, "タイムゾーンが変化しました", Toast.LENGTH_LONG).show()
+    }
+}
